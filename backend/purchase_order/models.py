f8f7fa67bdd959c5dbbc2052dae9b4f0c580fe84
Arshad <arshaarshad21@gmail.com>
2023-12-01 15:12:00 +0530

    created purchase_order app

---
+++ backend/purchase_order/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from vendor_profile.models import VendorProfile  
+
+
+class PurchaseOrder(models.Model):
+    po_number = models.CharField(max_length=255, unique=True)
+    vendor = models.ForeignKey(VendorProfile, on_delete=models.CASCADE)
+    order_date = models.DateTimeField()
+    delivery_date = models.DateTimeField()
+    items = models.JSONField()
+    quantity = models.IntegerField()
+    status = models.CharField(max_length=50)
+    quality_rating = models.FloatField(null=True, blank=True)
+    issue_date = models.DateTimeField()
+    acknowledgment_date = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return f"Purchase Order {self.po_number} for {self.vendor.name}"
